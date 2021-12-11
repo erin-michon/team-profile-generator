e58a679c24c5195c491e5aa6b3b775a9414d00ba
Erin M <erin.michon@gmail.com>
2021-12-11 15:21:20 -0500

    confirmed all inquirer prompts are working in index.js

--- index.js
+++ index.js
@@ -1,46 +1,80 @@
 //Application Dependencies
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
-// const Engineer = require('./lib/Engineer');
-// const Manager = require('./lib/Manager');
-// const Intern = require('./lib/Intern');
-
+const teamArr = [];
 
 //Questions for the Manager (initial prompt)
 const mgrQuestions = () => {
-    return inquirer
-    .prompt([
+
+    console.log(`
+    ==================
+    Add a Team Manager
+    ==================
+    `);
+
+    return inquirer.prompt([
         {
             type: 'input',
-            name: 'mgrName',
-            message: "Please enter the Manager's name.",
+            name: 'name',
+            message: "Please enter the Manager's name?",
         },
         {
             type: 'input',
-            name: 'mgrEmail',
+            name: 'id',
+            message: "Please enter the Manager's id number.",
+        },
+        {
+            type: 'input',
+            name: 'email',
             message: "Please enter the Manager's email address.",
         },
         {
             type: 'input',
-            name: 'mgrOffice',
+            name: 'officeNumber',
             message: "Please enter the Manager's office number.",
         },
+        {
+            type: 'list',
+            name: 'newEmployee',
+            choices: ["Intern", "Engineer", "The Team Is Complete!"],
+            message: "Please choose an employee to add to your team."
+        }
+    ])
+    .then(mgrData => {
+        const {name , id , email, officeNumber} = mgrData;
+        const manager = new Manager (name, id, email, officeNumber);
+        teamArr.push(manager);
 
-    ]);
+        if(mgrData.newEmployee === 'Intern') {
+            return internQuestions();
+        } else if (mgrData.newEmployee === 'Engineer') {
+            return engQuestions();
+        } else {
+            return //start generating file
+        }
+        
+    });
 };
 
 //Questions for the Engineer(if selected)
 const engQuestions = () => {
-    return inquirer
-    .prompt([
+    return inquirer.prompt([
         {
             type: 'input',
-            name: 'engName',
+            name: 'name',
             message: "Please enter the Engineer's name.",
         },
         {
             type: 'input',
-            name: 'engEmail',
+            name: 'id',
+            message: "Please enter the Engineers's id number.",
+        },
+        {
+            type: 'input',
+            name: 'email',
             message: "Please enter the Engineer's email address.",
         },
         {
@@ -48,22 +82,46 @@ const engQuestions = () => {
             name: 'github',
             message: "Please enter the Engineer's GitHub username.",
         },
+        {
+            type: 'list',
+            name: 'newEmployee',
+            choices: ["Intern", "Engineer", "The Team Is Complete!"],
+            message: "Please choose an employee to add to your team."
+        }
+    ])
+    .then(engData => {
+        const {name , id , email, github} = engData;
+        const engineer = new Engineer (name, id, email, github);
+        teamArr.push(engineer);
 
-    ]);
+        if(engData.newEmployee === 'Intern') {
+            return internQuestions();
+        } else if (engData.newEmployee === 'Engineer') {
+            return engQuestions();
+        } else {
+            return //start generating file
+        }
+        
+    });
+
 };
 
 //Questions for the Intern(if selected)
 const internQuestions = () => {
-    return inquirer
-    .prompt([
+    return inquirer.prompt([
         {
             type: 'input',
-            name: 'internName',
+            name: 'name',
             message: "Please enter the Intern's name.",
         },
         {
             type: 'input',
-            name: 'engEmail',
+            name: 'id',
+            message: "Please enter the Intern's id number.",
+        },
+        {
+            type: 'input',
+            name: 'email',
             message: "Please enter the Intern's email address.",
         },
         {
@@ -71,28 +129,53 @@ const internQuestions = () => {
             name: 'school',
             message: "Please enter the Intern's school.",
         },
+        {
+            type: 'list',
+            name: 'newEmployee',
+            choices: ["Intern", "Engineer", "The Team Is Complete!"],
+            message: "Please choose an employee to add to your team."
+        }
 
-    ]);
+    ])
+    .then(internData => {
+        const {name , id , email, school} = internData;
+        const intern = new Intern(name, id, email, school);
+        teamArr.push(intern);
+
+        if(internData.newEmployee === 'Intern') {
+            return internQuestions();
+        } else if (internData.newEmployee === 'Engineer') {
+            return engQuestions();
+        } else {
+            return //start generating file
+        }
+        
+    });
 };
 
 //Options Menu (initiated after Mgr Prompt)
-const userMenu = () => {
-    return inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'employee-type',
-            choices: [ "Intern", "Engineer", "The Team Is Complete!"],
-            message: "Please choose an employee to add to your team."
-        },
-    ]);
-};
+// const userMenu = () => {
+//     inquirer.prompt([
+//         {
+//             type: 'list',
+//             name: 'employeeType',
+//             choices: ["Intern", "Engineer", "The Team Is Complete!"],
+//             message: "Please choose an employee to add to your team."
+//         },
+//     ])
+//     .then( answer => {
+//         const {employeeType} = answer
 
-function createTeam() {
-    mgrQuestions();
+        
+       
+//     });
+
+// };
+
+const createTeam = () => {
+    mgrQuestions()
 };
 
 //Start Application
-createTeam();
+createTeam()
 
-module.exports = questions;
