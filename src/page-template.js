11f75e60c310c4e86850cbaa9b686678d2bccd1d
Erin M <erin.michon@gmail.com>
2021-12-12 13:08:36 -0500

    begin to design HTML output

--- src/page-template.js
+++ src/page-template.js
@@ -1,2 +1,42 @@
-//Generate pieces of HTML based on class
-//Put all HTML from user input onto the page
+//Generate sections based on class (employee type)
+//populate sections based on user input (TeamArr)
+//generate HTML page (export module)
+
+//Generate Manager Card
+
+const generateMgr = mgrData => {
+
+    return `
+    //create manager card
+    
+    `
+
+}
+
+/* Create Page:
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
+
+
+*/
